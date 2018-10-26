66b7bff8c7cbffb898b0e419de5b2c808a448e6b
Plinio Romero <romero.p.mfc@gmail.com>
2018-10-26 13:45:50 -0500

    new delas update sitemap

--- src/AppBundle/Controller/SitemapsController.php
+++ src/AppBundle/Controller/SitemapsController.php
@@ -18,7 +18,7 @@ class SitemapsController extends Controller
 
         $urls[] = array(
             'loc' => $this->get('router')->generate('homepage'),
-            'lastmod' => "2018-10-24",
+            'lastmod' => "2018-10-26",
             'changefreq' => 'daily',
             'priority' => '1.0'
         );
