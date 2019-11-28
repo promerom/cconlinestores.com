a99afddb24931f1a2db0eb5c2c5bb7d25e8667e1
Plinio Romero <romero.p.mfc@gmail.com>
2019-11-28 14:02:13 -0500

    delete marker config faile and move to packages/dev add method for parse data from ktronix

--- src/AppBundle/Controller/CrawlerController.php
+++ src/AppBundle/Controller/CrawlerController.php
@@ -44,76 +44,7 @@ class CrawlerController extends Controller
 
         $nodeValues = $crawler->filter('.products-grid .item')->each(function (Crawler $node, $i) use ($_idStore, $_idBrand, $_idCurrency) {
 
-            $productName = $node->filter(".product-name a");
-            $productImg = $node->filter(".product-image img");
-            $oldPrice = $node->filter(".price-box .old-price .price-old");
-            $specialPrice = $node->filter(".price-box .special-price .price .price");
-            $regularPrice = $node->filter(".price-box .regular-price .price .price");
-
-            $product = new Product();
-            $doctrine = $this->getDoctrine();
-
-            if ($productName->count() > 0) {
-                $name = $productName->attr("title");
-                $url = $productName->attr("href");
-
-                $items = $doctrine->getRepository("AppBundle:Product")->findByUrl($url);
-                if (empty($items)) {
-                    $tmp = true;
-                }
-
-                $product->setName($name);
-                $product->setUrl($url);
-            }
-
-            if ($productImg->count() > 0) {
-                $image = $productImg->attr("data-src");
-                $product->setImage($image);
-            }
-
-//             For delete special characteres in prices
-            $array = array("$", ".", " ", "\n", chr(0xC2).chr(0xA0));
-            if ($oldPrice->count() > 0) {
-                $oldPrice = $oldPrice->text();
-
-                $oldPrice = str_replace($array, "", $oldPrice);
-                $product->setOldPrice(trim($oldPrice));
-            }
-
-            if ($specialPrice->count() > 0) {
-                $specialPrice = $specialPrice->text();
-
-                $specialPrice = str_replace($array, "", $specialPrice);
-                $product->setSpecialPrice($specialPrice);
-            }
-
-            if ($regularPrice->count() > 0) {
-                $price = $regularPrice->text();
-
-                $price = str_replace($array, "", $price);
-                $product->setPrice($price);
-            }
-
-            $currency = $doctrine
-            ->getRepository('AppBundle:Currency')
-            ->find($_idCurrency);
-            $product->setCurrency($currency);
-
-            $brand = $doctrine
-            ->getRepository('AppBundle:Brand')
-            ->find($_idBrand);
-            $product->setBrand($brand);
-
-            $store = $doctrine
-            ->getRepository('AppBundle:Store')
-            ->find($_idStore);
-            $product->setStore($store);
-
-            if (isset($tmp)) {
-                $em = $doctrine->getManager();
-                $em->persist($product);
-                $em->flush();
-            }
+            $product = $this->parseDataFromKtronix($node, $_idStore, $_idBrand, $_idCurrency);
 
             return $product->getName();
 
@@ -127,6 +58,81 @@ class CrawlerController extends Controller
 //         return $this->render('AppBundle:Crawler:get_data.html.twig', array(
             // ...
 //         ));
+    }
+
+    private function parseDataFromKtronix($node, $_idStore, $_idBrand, $_idCurrency) {
+        $productName = $node->filter(".product-name a");
+        $productImg = $node->filter(".product-image img");
+        $oldPrice = $node->filter(".price-box .old-price .price-old");
+        $specialPrice = $node->filter(".price-box .special-price .price .price");
+        $regularPrice = $node->filter(".price-box .regular-price .price .price");
+
+        $product = new Product();
+        $doctrine = $this->getDoctrine();
+
+        if ($productName->count() > 0) {
+            $name = $productName->attr("title");
+            $url = $productName->attr("href");
+
+            $items = $doctrine->getRepository("AppBundle:Product")->findByUrl($url);
+            if (empty($items)) {
+                $tmp = true;
+            }
+
+            $product->setName($name);
+            $product->setUrl($url);
+        }
+
+        if ($productImg->count() > 0) {
+            $image = $productImg->attr("data-src");
+            $product->setImage($image);
+        }
+
+        //             For delete special characteres in prices
+        $array = array("$", ".", " ", "\n", chr(0xC2).chr(0xA0));
+        if ($oldPrice->count() > 0) {
+            $oldPrice = $oldPrice->text();
+
+            $oldPrice = str_replace($array, "", $oldPrice);
+            $product->setOldPrice(trim($oldPrice));
+        }
+
+        if ($specialPrice->count() > 0) {
+            $specialPrice = $specialPrice->text();
+
+            $specialPrice = str_replace($array, "", $specialPrice);
+            $product->setSpecialPrice($specialPrice);
+        }
+
+        if ($regularPrice->count() > 0) {
+            $price = $regularPrice->text();
+
+            $price = str_replace($array, "", $price);
+            $product->setPrice($price);
+        }
+
+        $currency = $doctrine
+        ->getRepository('AppBundle:Currency')
+        ->find($_idCurrency);
+        $product->setCurrency($currency);
+
+        $brand = $doctrine
+        ->getRepository('AppBundle:Brand')
+        ->find($_idBrand);
+        $product->setBrand($brand);
+
+        $store = $doctrine
+        ->getRepository('AppBundle:Store')
+        ->find($_idStore);
+        $product->setStore($store);
+
+        if (isset($tmp)) {
+            $em = $doctrine->getManager();
+            $em->persist($product);
+            $em->flush();
+        }
+
+        return $product;
     }
 
 }
