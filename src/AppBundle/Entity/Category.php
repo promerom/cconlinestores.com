f1075a977a0d0e2927e03434c68b488ff8c84790
Plinio Romero <romero.p.mfc@gmail.com>
2019-12-13 15:23:31 -0500

    fix url name generate

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -88,9 +88,11 @@ class Category
     {
         $this->name = $name;
 
-        $search = array(" ", "'", '"', "+", "*", "´", "(", ")", "?", "¿", "¡", "!");
+        $search = array(" ", "'", '"', "+", "*", "´", "(", ")", "?", "¿", "¡", "!", "/");
         $replace = array("-", "");
-        $urlName = strtolower(str_replace($search, $replace, $name));
+        $urlName = str_replace($search, $replace, $name);
+        $urlName = $this->removeAccents($urlName);
+        $urlName = strtolower($urlName);
 
         $this->setUrlName($urlName);
 
@@ -220,5 +222,47 @@ class Category
         $this->urlName = $urlName;
 
         return $this;
+    }
+
+    private function removeAccents($string)
+    {
+
+        //Codificamos la cadena en formato utf8 en caso de que nos de errores
+        //         $string = utf8_encode($string);
+
+        //Ahora reemplazamos las letras
+        $string = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $string
+            );
+
+        $string = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $string );
+
+        $string = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $string );
+
+        $string = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $string );
+
+        $string = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $string );
+
+        $string = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C'),
+            $string
+            );
+
+        return $string;
     }
 }
