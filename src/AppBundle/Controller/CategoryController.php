a8528782dd5866b87a9ca64a4d8fa1953aecc1a3
Plinio Romero <romero.p.mfc@gmail.com>
2019-12-17 15:16:38 -0500

    add category page

---
+++ src/AppBundle/Controller/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * @Route(
+     *      "/{_category}",
+     *      name="category",
+     *      requirements={
+     *          "_category"="celulares|televisores|computadores"
+     *      }
+     * )
+     */
+    public function index(Request $request, $_route_params)
+    {
+        $doctrine = $this->getDoctrine();
+
+        /**
+         * @var Category $category
+         */
+        $category = $doctrine->getRepository("AppBundle:Category")->findOneByUrlName($_route_params["_category"]);
+
+        return $this->render('controller/category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+            'category' => $category,
+            'products' => $category->getProducts()
+        ]);
+    }
+}
