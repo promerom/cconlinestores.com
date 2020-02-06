a4f82685f12d92f25ac12788f8150d42bad8b6fe
Plinio Romero <romero.p.mfc@gmail.com>
2020-02-06 17:10:14 -0500

    validate when is not new items to publish on fb

--- src/AppBundle/Controller/AutoPostController.php
+++ src/AppBundle/Controller/AutoPostController.php
@@ -26,63 +26,67 @@ class AutoPostController extends Controller
          */
         $item = $doctrine->getRepository("AppBundle:Product")->findOneBy(array("postOnFacebook" => null), array("modified" => "DESC"));
 
-        $url = $this->get('router')->generate('product_detail',
-            array(
-                '_category' => $item->getCategory()->getUrlName(),
-                '_url_name' => $item->getUrlName(),
-                '_id' => $item->getId()
-            ),
-            UrlGenerator::ABSOLUTE_URL);
+        if (!is_null($item)){
+            $url = $this->get('router')->generate('product_detail',
+                array(
+                    '_category' => $item->getCategory()->getUrlName(),
+                    '_url_name' => $item->getUrlName(),
+                    '_id' => $item->getId()
+                ),
+                UrlGenerator::ABSOLUTE_URL);
 
-//         $url = str_replace("http://cconlinestores.lo", "https://cconlinestores.com", $url);
-//         $url = str_replace("193", "1", $url);
-//         $url = str_replace("236", "42", $url);
+    //         $url = str_replace("http://cconlinestores.lo", "https://cconlinestores.com", $url);
+    //         $url = str_replace("193", "1", $url);
+    //         $url = str_replace("236", "42", $url);
 
-        if ($item->getSpecialPrice() != null) {
-            $price = $item->getSpecialPrice();
-        } elseif ($price = $item->getOldPrice() != null) {
-            $price = $item->getOldPrice();
-        } elseif ($price = $item->getPrice() != null) {
-            $price = $item->getPrice();
-        }
-
-        $format = new \NumberFormatter("es", \NumberFormatter::CURRENCY);
-        $formatPrice = $format->formatCurrency($price, $item->getCurrency()->getDescription());
-        $symbol = $item->getCurrency()->getSymbol();
-        $formatPrice = $symbol . $formatPrice;
-
-        $hashtags = "#";
-        $hashtags .= $item->getCategory()->getName();
-        $hashtags .= " #" . $item->getBrand()->getName();
-        $hashtags .= " #" . $item->getStore()->getName();
-
-        $contentMessage = $item->getName() . " " . $formatPrice . " " . $hashtags;
-
-        if (!$item->getPostOnFacebook()) {
-            $msg = new Message($contentMessage, $url);
-            $msg->setNetworksToPublishOn([Enum::FACEBOOK]);
-
-            try {
-                $responseMsg = $this->get('social_post')->publish($msg);
-            } catch (FailureWhenPublishingMessage $e) {
-                $response = new Response(json_encode($contentMessage . " ERROR: " . $e->getMessage()));
-                $response->headers->set('Content-Type', 'application/json');
-
-                return $response;
+            if ($item->getSpecialPrice() != null) {
+                $price = $item->getSpecialPrice();
+            } elseif ($price = $item->getOldPrice() != null) {
+                $price = $item->getOldPrice();
+            } elseif ($price = $item->getPrice() != null) {
+                $price = $item->getPrice();
             }
 
-            $item->setPostOnFacebook(true);
-            $em = $doctrine->getManager();
-            $em->persist($item);
-            $em->flush();
+            $format = new \NumberFormatter("es", \NumberFormatter::CURRENCY);
+            $formatPrice = $format->formatCurrency($price, $item->getCurrency()->getDescription());
+            $symbol = $item->getCurrency()->getSymbol();
+            $formatPrice = $symbol . $formatPrice;
 
-            $response = new Response(json_encode($contentMessage . " RESPONSE: " . $responseMsg));
-            $response->headers->set('Content-Type', 'application/json');
+            $hashtags = "#";
+            $hashtags .= $item->getCategory()->getName();
+            $hashtags .= " #" . $item->getBrand()->getName();
+            $hashtags .= " #" . $item->getStore()->getName();
+
+            $contentMessage = $item->getName() . " " . $formatPrice . " " . $hashtags;
+
+            if (!$item->getPostOnFacebook()) {
+                $msg = new Message($contentMessage, $url);
+                $msg->setNetworksToPublishOn([Enum::FACEBOOK]);
+
+                try {
+                    $responseMsg = $this->get('social_post')->publish($msg);
+                } catch (FailureWhenPublishingMessage $e) {
+                    $response = new Response(json_encode($contentMessage . " ERROR: " . $e->getMessage()));
+                    $response->headers->set('Content-Type', 'application/json');
+
+                    return $response;
+                }
+
+                $item->setPostOnFacebook(true);
+                $em = $doctrine->getManager();
+                $em->persist($item);
+                $em->flush();
+
+                $response = new Response(json_encode($contentMessage . " RESPONSE: " . $responseMsg));
+                $response->headers->set('Content-Type', 'application/json');
+            } else {
+                $response = new Response(json_encode($item->getName() . " ya fue publicado en FB"));
+                $response->headers->set('Content-Type', 'application/json');
+            }
         } else {
-            $response = new Response(json_encode($item->getName() . " ya fue publicado en FB"));
+            $response = new Response("No hay items nuevos para publicar en FB"));
             $response->headers->set('Content-Type', 'application/json');
         }
-
         return $response;
     }
 
