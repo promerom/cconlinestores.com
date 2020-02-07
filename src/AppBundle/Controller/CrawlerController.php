a6f828c2c5cb55c4dd0627c4b0dd27cb4ff944d2
Plinio Romero <romero.p.mfc@gmail.com>
2020-02-07 15:34:24 -0500

    add crawler for apple smartphones strip special chars in name

--- src/AppBundle/Controller/CrawlerController.php
+++ src/AppBundle/Controller/CrawlerController.php
@@ -75,6 +75,18 @@ class CrawlerController extends Controller
                 $_idCategory = $category->getId();
 
                 break;
+            case "ktr_apple_cel":
+                $url = "https://www.ktronix.com/telefonos-celulares/ver/apple";
+                $store = $doctrine->getRepository('AppBundle:Store')->findOneByName("Ktronix");
+                $_idStore = $store->getId();
+                $brand = $doctrine->getRepository('AppBundle:Brand')->findOneByName("Apple");
+                $_idBrand = $brand->getId();
+                $currency = $doctrine->getRepository('AppBundle:Currency')->findOneByName("Pesos");
+                $_idCurrency = $currency->getId();
+                $category = $doctrine->getRepository('AppBundle:Category')->findOneByName("Celulares");
+                $_idCategory = $category->getId();
+
+                break;
             default:
                 $url = "https://www.ktronix.com/telefonos-celulares/celulares-libres/samsung";
                 break;
